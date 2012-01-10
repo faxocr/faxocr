65c6d1da2746e106cc1a51ed33e2822886af829f
Takashi Okumura <taka@wide.ad.jp>
2012-01-10 17:59:29 +0900

    test script looks okay now.

--- faxocr/app/external/test.rb
+++ faxocr/app/external/test.rb
@@ -7,39 +7,60 @@ require "yaml"
 
 rails_prefix = ARGV[0] || "./"
 group = ARGV[1] || exit(0)
-survey = ARGV[2] || exit(0) # XXX
+survey = ARGV[2] || exit(0) # XXX: is this needed?
 
 config_db = rails_prefix + "/config/database.yml"
-db_env = ""
+# XXX
+#db_env = "" 
+db_env = "development"
 cellinfo = Hash.new()
 
 ActiveRecord::Base.configurations = YAML.load_file(config_db)
 ActiveRecord::Base.establish_connection(db_env)
+
 Dir.glob(rails_prefix + '/app/models/*.rb').each do |model|
   load model
+end
+
+# Initialization
+@group = Group.find(group)
+
+#
+# create a default candidate (hardcoded)
+#
+@candidate = @group.candidates.build
+@candidate.candidate_code = "00000" 		# (string, not null)
+@candidate.candidate_name = "一般報告者"	# (string, not null)
+@candidate.group_id = group			# (int, not null)
+@candidate.tel_number = "03-1111-1111"		# (string)
+@candidate.fax_number = "03-1111-1111"		# (string)
+if @candidate.save
+  print "default candidate: success\n"
+else
+  print "default candidate: fail\n"
 end
 
 #
 # create a new survey
 #
-do
-  @group = Group.find(group)
-  @survey = @group.surveys.build(survey)
-  @survey.report_header = ""
-  @survey.report_footer = ""
+@survey = @group.surveys.build
+@survey.survey_name = "自動生成サーベイ" # XXX
+@survey.status = 0 # XXX: NEEDS INITIAL VALUE HERE
+@survey.report_header = ""
+@survey.report_footer = ""
 
-  @candidates = @group.candidates
-  @candidates.each do |candidate|
-    survey_candidate = SurveyCandidate.new
-    survey_candidate.candidate_id = candidate.id
-    survey_candidate.role = 'sr'
-    @survey.survey_candidates << survey_candidate
-  end
-  if @survey.save
-    print "success"
-  else
-    print "fail"
-  end
+@candidates = @group.candidates
+@candidates.each do |candidate|
+  survey_candidate = SurveyCandidate.new
+  survey_candidate.candidate_id = candidate.id
+  survey_candidate.role = 'sr'
+  @survey.survey_candidates << survey_candidate
+end
+if @survey.save
+  print "survey candidate: success\n"
+else
+  print "survey candidate: fail\n"
+  exit(0)
 end
 
 #
@@ -49,76 +70,90 @@ end
 #
 # [Property]
 #
-# survey_property.ocr_name
-# survey_property.ocr_name_full
-# survey_property.view_order
-# survey_property.data_type
-# sheet_property.position_x
-# sheet_property.position_y
-# sheet_property.colspan
+# [0] survey_property.ocr_name
+# [1] survey_property.ocr_name_full
+# [2] survey_property.view_order
+# [3] survey_property.data_type
+# [4] sheet_property.position_x
+# [5] sheet_property.position_y
+# [6] sheet_property.colspan
 
 props = []
-props << ["テスト", "テスト", 1, "number", 1, 1, 1]
-props << ["フォーム１", "フォーム１", 1, "number", 2, 2, 1]
-props << ["テスト", "テスト", 1, "number", 3, 3, 1]
-props << ["フォーム３", "フォーム３", 1, "number", 4, 4, 1]
-props << ["フォーム２", "フォーム２", 1, "number", 5, 5, 1]
+props << ["テスト", "テスト", 1, "number", 1, 11, 1]
+props << ["フォーム１", "フォーム１", 1, "number", 2, 12, 1]
+props << ["テスト2",     "テストル",   1, "number", 3, 13, 2]
+props << ["フォーム３", "フォーム３", 1, "number", 4, 14, 1]
+props << ["フォーム２", "フォーム２", 1, "number", 5, 15, 1]
 
 props.each do |prop|
 
   # object building
   @survey_property = @survey.survey_properties.build
   @survey_property.survey_id = survey		# integer
-  @survey_property.ocr_name = prop[0]		# string
+  @survey_property.ocr_name = prop[0]		# string (must be unique!)
   @survey_property.ocr_name_full = prop[1]	# string
   @survey_property.view_order = prop[2]		# integer
   @survey_property.data_type = prop[3]		# string
+  # print prop[0] + "/" + prop[1] + "\n"
 
   # save
   if @survey_property.save
-    print  prop[0] + " success"
+    print  "survey property " + prop[0] + ": success\n"
     cellinfo[@survey_property.id] = [prop[4], prop[5], prop[6]]
   else
-    print  prop[0] + " fail"
+    print  "survey property " + prop[0] + ": fail\n"
+    exit(0)
   end
 end
 
 #
 # create sheet/property mapping
 #
-do
-  @group = Group.find(params[:group_id])
-  @survey = @group.surveys.find(params[:survey_id])
-  @sheet = @survey.sheets.build(params[:sheet])
+@sheet = @survey.sheets.build
 
-  # sheet作成
-  @sheet.sheet_code = "自動シート" + params[:survey_id].to_s
-  @sheet.sheet_name = "自動シート" + params[:survey_id].to_s
-  @sheet.survey_id = params[:survey_id]
-  @sheet.block_width = 20 # XXX
-  @sheet.block_height = 10 # XXX
-  @sheet.status = 1
+# sheet作成
+@sheet.sheet_code = "自動生成シート" + survey.to_s
+@sheet.sheet_name = "自動生成シート" + survey.to_s
+@sheet.survey_id = survey
+@sheet.block_width = 20 # XXX
+@sheet.block_height = 10 # XXX
+@sheet.status = 1
+# save
+if @sheet.save
+  print  "sheet: save success\n"
+else
+  print  "sheet: save fail\n"
+  exit(0)
+end
 
-  # survey_property作成
-  survey_properties = @survey.survey_properties
-  survey_properties.each do |survey_property|
+#
+# sheet_property generation
+#
+survey_properties = @survey.survey_properties
+survey_properties.each do |@survey_property|
 
-    prop = cellinfo[survey_property.id]
-
-    sheet_property = SheetProperty.new
-    sheet_property.survey_property_id = survey_property.id # survey_propertyからコピー
-    sheet_property.cellinfo_x = prop[0]
-    sheet_property.cellinfo_y = prop[1]
-    sheet_property.colspan = prop[2]
-    @sheet.sheet_properties << sheet_property
+  prop = cellinfo[@survey_property.id]
+  if prop.nil? then
+    next
   end
+  # survey_propertyからコピー
+  sheet_property = SheetProperty.new
+  sheet_property.sheet_id = @sheet.object_id
+  sheet_property.survey_property_id = @survey_property.id
+  sheet_property.position_x = prop[0]
+  sheet_property.position_y = prop[1]
+  sheet_property.colspan = prop[2]
+  @sheet.sheet_properties << sheet_property
 
-  # save
-  if @sheet.save
-    print  "survey_property: save success"
-  else
-    print  "survey_property: save fail"
-  end
+  # print "> " + prop[0].to_s + "/" + prop[1].to_s + "/" + prop[2].to_s + "\n"
+end
+
+# save
+if @sheet.save
+  print  "sheet_property: save success\n"
+else
+  print  "sheet_property: save fail\n"
+  exit(0)
 end
 
 exit(0)
