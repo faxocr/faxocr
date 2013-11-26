6441378ac68d69c8ba8e3c317449c1c3b8904227
Taketo Takashima <t.taketo1113@gmail.com>
2013-11-27 00:03:25 +0900

    Added a necessary column at group form

--- rails/app/models/group.rb
+++ rails/app/models/group.rb
@@ -4,6 +4,8 @@ class Group < ActiveRecord::Base
   has_many  :role_mappings
   has_many  :users, :through => :role_mappings
 
+  validates_presence_of :group_name
+
   def is_administrator?
     if self.id == 1
       return true
