9f42619bd3fb601764abee5e7a38734520eaf6f9
Sohgo Takeuchi <sohgo@sohgo.dyndns.org>
2014-02-27 18:39:58 +0900

    bump the passenger's version from 4.0.20 to 4.0.37

--- setup/chef-solo/cookbooks/faxocr/attributes/passenger.rb
+++ setup/chef-solo/cookbooks/faxocr/attributes/passenger.rb
@@ -1,5 +1,5 @@
 
-default[:faxocr][:passenger_version]   = "4.0.20"
+default[:faxocr][:passenger_version]   = "4.0.37"
 default[:faxocr][:passenger_root_path]   = "/usr/lib/ruby/gems/1.8/gems/passenger-#{node[:faxocr][:passenger_version]}"
 default[:faxocr][:passenger_module_path] = "/usr/lib/ruby/gems/1.8/gems/passenger-#{node[:faxocr][:passenger_version]}/buildout/apache2/mod_passenger.so"
 default[:faxocr][:passenger_ruby_bin] = "/usr/bin/ruby1.8"
