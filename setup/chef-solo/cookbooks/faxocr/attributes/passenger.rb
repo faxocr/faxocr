63e28969b17fd570fc3887c9334f963a64730f59
Sohgo Takeuchi <sohgo@sohgo.more.jp>
2014-08-07 14:46:35 +0900

    upgrade passenger from 4.0.45 to 4.0.48

--- setup/chef-solo/cookbooks/faxocr/attributes/passenger.rb
+++ setup/chef-solo/cookbooks/faxocr/attributes/passenger.rb
@@ -1,5 +1,5 @@
 
-default[:faxocr][:passenger_version]   = "4.0.45"
+default[:faxocr][:passenger_version]   = "4.0.48"
 default[:faxocr][:passenger_root_path]   = "/var/lib/gems/1.8/gems/passenger-#{node[:faxocr][:passenger_version]}"
 default[:faxocr][:passenger_module_path] = "/var/lib/gems/1.8/gems/passenger-#{node[:faxocr][:passenger_version]}/buildout/apache2/mod_passenger.so"
 default[:faxocr][:passenger_ruby_bin] = "/usr/bin/ruby1.8"
