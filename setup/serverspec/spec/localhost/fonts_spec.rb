32833cd3599c13f40d5e0bd5621c7b0614c848fc
Sohgo Takeuchi <sohgo@sohgo.more.jp>
2014-08-07 13:15:35 +0900

    serverspec: check fonts on only debian platform
    This font config files are installed on only debian platform.

--- setup/serverspec/spec/localhost/fonts_spec.rb
+++ setup/serverspec/spec/localhost/fonts_spec.rb
@@ -7,12 +7,12 @@ require 'spec_helper'
 end
 
 %w{65-fonts-takao-gothic.conf 65-fonts-takao-mincho.conf 65-fonts-takao-pgothic.conf 69-msfonts-to-takao.conf}.each do |file|
-  describe file("/etc/fonts/conf.avail/#{file}") do
+  describe file("/etc/fonts/conf.avail/#{file}"), :if => os['family'] == 'Debian' do
     it { should be_file }
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
   end
-  describe file("/etc/fonts/conf.d/#{file}") do
+  describe file("/etc/fonts/conf.d/#{file}"), :if => os['family'] == 'Debian' do
     it { should be_linked_to "/etc/fonts/conf.avail/#{file}"}
   end
 end
