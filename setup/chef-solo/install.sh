646678ccafe139d20828b63e3f0fe6fbd17b75db
Sohgo Takeuchi <sohgo@sohgo.more.jp>
2015-01-05 17:25:43 +0900

    Lock the version of chef, serverspec and dependent gems
    To correctly work chef and serverspec with ruby 1.8.7, lock the version of them and dependent gems.

--- setup/chef-solo/install.sh
+++ setup/chef-solo/install.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
-aptitude update
-aptitude upgrade -y
+apt-get update
+apt-get upgrade -y
 
-aptitude install -y git
+apt-get install -y git wget
 
-aptitude install -y rubygems1.8 ruby1.8-dev
-gem1.8 install chef --no-ri --no-rdoc
-gem1.8 install knife-solo --no-ri --no-rdoc
+apt-get install -y rubygems1.8 ruby1.8-dev
+gem1.8 install bundler --no-ri --no-rdoc
+
+wget -q http://faxocr.googlecode.com/git-history/master/setup/chef-solo/Gemfile
+wget -q http://faxocr.googlecode.com/git-history/master/setup/chef-solo/Gemfile.lock
+bundle install
 
 git clone https://code.google.com/p/faxocr
 sed -i'' -e "s#%%EDIT_ME%%#`pwd`#" faxocr/setup/chef-solo/solo.rb
-(cd faxocr; \
-git submodule init; \
-git submodule update; \
-)
-(cd faxocr/setup/chef-solo; \
-chef-solo -c `pwd`/solo.rb -j `pwd`/nodes/localhost.json; \
+(cd faxocr;
+git submodule init;
+git submodule update;
 )
 
-gem1.8 install serverspec
-(cd faxocr/setup/serverspec; \
-rake spec; \
+(cd faxocr/setup/chef-solo;
+chef-solo -c `pwd`/solo.rb -j `pwd`/nodes/localhost.json;
+)
+
+(cd faxocr/setup/serverspec;
+rake spec;
 )
