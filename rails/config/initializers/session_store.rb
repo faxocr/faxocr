5ce142f8261459c4c0023a9cb116179accf1485c
Sohgo Takeuchi <sohgo@sohgo.more.jp>
2015-06-26 14:49:45 +0900

    update and import files from Rails 3.0.x's default

--- rails/config/initializers/session_store.rb
+++ rails/config/initializers/session_store.rb
@@ -11,5 +11,5 @@ ActionController::Base.session = {
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
+# (create the session table with "rails generate session_migration")
 # ActionController::Base.session_store = :active_record_store
