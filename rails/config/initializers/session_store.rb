692912403646ea0d0b215e7c9ebf2b29b5dc4067
Sohgo Takeuchi <sohgo@sohgo.more.jp>
2015-07-21 15:48:51 +0900

    save session info into the DB to avoid from error

--- rails/config/initializers/session_store.rb
+++ rails/config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Faxocr::Application.config.session_store :cookie_store, key: '_faxocr_session'
+#Faxocr::Application.config.session_store :cookie_store, key: '_faxocr_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Faxocr::Application.config.session_store :active_record_store
+Faxocr::Application.config.session_store :active_record_store
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -1,15 +1,17 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150408114440) do
+ActiveRecord::Schema.define(:version => 20150710043900) do
 
   create_table "answer_sheet_properties", :force => true do |t|
     t.integer  "answer_sheet_id", :null => false
@@ -69,6 +71,16 @@ ActiveRecord::Schema.define(:version => 20150408114440) do
   end
 
   add_index "role_mappings", ["group_id", "user_id"], :name => "index_role_mappings_on_group_id_and_user_id", :unique => true
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sheet_cellattribute_colwidths", :force => true do |t|
     t.integer  "sheet_cellattribute_id", :null => false
