5123b4a8d86d76c473db9d48df34e11cf0adf922
Sohgo Takeuchi <sohgo@sohgo.more.jp>
2015-08-20 14:00:52 +0900

    fixed generated sql error
    Mysql2::Error: Unknown column 'answer_sheet_properties.view_order' in
    'order clause': SELECT `answer_sheet_properties`.* FROM
    `answer_sheet_properties` INNER JOIN survey_properties ON
    answer_sheet_properties.ocr_name = survey_properties.ocr_name AND
    survey_properties.survey_id = 167 WHERE
    `answer_sheet_properties`.`answer_sheet_id` = 120  ORDER BY
    `answer_sheet_properties`.`view_order` ASC

--- rails/app/controllers/answer_sheets_controller.rb
+++ rails/app/controllers/answer_sheets_controller.rb
@@ -37,7 +37,7 @@ class AnswerSheetsController < ApplicationController
 
     begin
     @answer_sheet = AnswerSheet.find(params[:id])
-    @answer_sheet_properties = @answer_sheet.answer_sheet_properties.joins("INNER JOIN survey_properties ON answer_sheet_properties.ocr_name = survey_properties.ocr_name AND survey_properties.survey_id = " + @survey.id.to_s).order(:view_order).sort_by{|asp| asp.ocr_value}
+    @answer_sheet_properties = @answer_sheet.answer_sheet_properties.joins("INNER JOIN survey_properties ON answer_sheet_properties.ocr_name = survey_properties.ocr_name AND survey_properties.survey_id = " + @survey.id.to_s).order('survey_properties.view_order').sort_by{|asp| asp.ocr_value}
     sheets = @survey.sheets
     sheet = sheets.find(@answer_sheet.sheet_id)
     rescue
