2a31c4d93dba6a0d8b4c3d163b8c958843f1591a
Sohgo Takeuchi <sohgo@sohgo.more.jp>
2015-08-24 17:03:21 +0900

    rake rails:update

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -25,5 +25,8 @@ module Faxocr
     config.encoding = "utf-8"
 
     config.action_controller.action_on_unpermitted_parameters = :raise
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
