2c72f36a5b957655a1d1748d65432e0fc29e76ee
Sohgo Takeuchi <sohgo@sohgo.more.jp>
2016-05-27 10:41:21 +0900

    setup: can configure git repository and branch

--- setup/chef-solo/cookbooks/faxocr/attributes/default.rb
+++ setup/chef-solo/cookbooks/faxocr/attributes/default.rb
@@ -1,5 +1,7 @@
 
 default[:faxocr][:home_dir]      = '/home/faxocr'
 default[:faxocr][:setup_mode]    = :initial_setup # or :production_update
+default[:faxocr][:repository]    = 'https://github.com/faxocr/faxocr'
+default[:faxocr][:branch]        = :master
 
 # vim:set expandtab shiftwidth=2 tabstop=2 softtabstop=2:
